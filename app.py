5b1160b866348e35a120c9d16b0dd48692ce1638
Zander Meitus <zmeitus@uchicago.edu>
2022-03-09 09:08:28 -0600

    add basic dash skeleton with dummy data

---
+++ app.py
@@ -0,0 +1,31 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+
+df = pd.read_csv('dummy_data.csv')
+
+fig = px.bar(df, x="dc_id", y="aid", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='(la)Monty Python Dash Template'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
