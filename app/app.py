6a55d49a8165b0d8af00aabda786f616afd9a3ad
Zander Meitus <zmeitus@uchicago.edu>
2022-03-10 18:18:36 -0600

    add parall coord chart

--- app/app.py
+++ app/app.py
@@ -8,74 +8,78 @@ import pandas as pd
 
 app = Dash(__name__)
 
-colors = {
-    'background': 'white',
-    'text': '#af7ac5  '
-}
-
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
 df = pd.read_csv('dummy_data.csv')
 
+pc_fig = px.parallel_coordinates(df, color="aid",
+                              dimensions=df.columns[7:11])
+#fig_bar = px.bar(df, x="dc_id", y="aid", color='dem_rep', barmode="group")
+#
+#fig_bar.update_layout(
+#    plot_bgcolor=colors['background'],
+#    paper_bgcolor=colors['background'],
+#    font_color=colors['text']
+#)
 
-fig_bar = px.bar(df, x="dc_id", y="aid", color='dem_rep', barmode="group")
-
-fig_bar.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+app.layout = html.Div(children=[
     html.H1(
         children='(la)Monty Python Dash Template',
         style={
-            'textAlign': 'center',
-            'color': colors['text']
+            'textAlign': 'center'
         }
     ),
 
-    html.Div(children=[
-        html.Label('Select a State'),
-        dcc.Dropdown(df['state'].unique(), 
-            df['state'].unique()[0:3], multi = True, id='state-dd'),
-        html.Label('Select a Year'),
-        dcc.Dropdown(df['year'].unique(), 
-            df['year'].unique()[0], multi = True, id='year-dd'),
-        html.Label('Select Disaster Type'),
-        dcc.Dropdown(df['disaster_type'].unique(), 
-            df['disaster_type'].unique()[0], multi = True, id='disaster-dd'),
-        html.Label('Select X Axis Variable'),
-        dcc.Dropdown(df.columns[7:11], df.columns[7], id='xaxis-dd')
-    ]),
+    html.Div(className = 'filter-container',
+        children=[
+            html.Div(className ='filter-div', 
+                children = [html.Label('Select a State'),
+                    dcc.Dropdown(df['state'].unique(), 
+                    df['state'].unique()[0:3], multi = True, id='state-dd')]),
+            html.Div(className='filter-div',
+                children = [html.Label('Select a Year'),
+                    dcc.Dropdown(df['year'].unique(), 
+                    df['year'].unique()[0], multi = True, id='year-dd')])
+        ]
+    ),
+    html.Div(className = 'filter-container',
+        children=[
+            html.Div(className='filter-div',
+                children = [html.Label('Select Disaster Type'),
+                    dcc.Dropdown(df['disaster_type'].unique(), 
+                    df['disaster_type'].unique()[0], multi = True, id='disaster-dd')]
+            ),
+            html.Div(className='filter-div',
+                    children=[html.Label('Select X Axis Variable'),
+                    dcc.Dropdown(df.columns[7:11], df.columns[7], id='xaxis-dd')]
+            )
+        ]
+    ),
 
     html.Div(children=[
         dcc.Graph(
             id='scatter-fig'
         )
     ]),
-
-
-    html.H3(children='A table of dummy data',
-        style={
-        'textAlign': 'center',
-        'color': colors['text']
-        }
-    ),
-    generate_table(df)
+    html.Div(children=[
+        dcc.Graph(
+            id='pc-fig',
+            figure=pc_fig
+        )
+    ])
 ])
 
 
 @app.callback(
     Output('scatter-fig', 'figure'),
+    Output('pc-fig', 'figure'),
     Input('state-dd', 'value'),
     Input('year-dd', 'value'),
     Input('disaster-dd', 'value'),
     Input('xaxis-dd', 'value')
 )
-def update_figures(states, years, disasters, xaxis):
-    # print('states',states)
+def update_scatter(states, years, disasters, xaxis):
     if not isinstance(states, list):
         states = [states]
     if not isinstance(years, list):
@@ -84,16 +88,19 @@ def update_figures(states, years, disasters, xaxis):
         disasters = [disasters]
 
     filtered_df = df[df['state'].isin(states) & 
-                    df['disaster_type'].isin(disasters) & 
-                    df['year'].isin(years)]
+        df['disaster_type'].isin(disasters) & 
+        df['year'].isin(years)]
 
-    fig = px.scatter(filtered_df, x=xaxis, y="aid",
-                size="population", color="disaster_type", hover_name="county_id",
-                size_max=60)
+    scatter_fig = px.scatter(filtered_df, x=xaxis, y="aid",
+        size="population", color="disaster_type", hover_name="county_id",
+        size_max=60)
 
-    fig.update_layout()
+    pc_fig = px.parallel_coordinates(filtered_df, color="aid",
+                              dimensions=df.columns[7:11])
 
-    return fig
+    scatter_fig.update_layout()
+
+    return scatter_fig, pc_fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
