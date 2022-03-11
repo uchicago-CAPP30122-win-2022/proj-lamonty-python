7406ed43f280a893355dc2c36f67c68730deb213
Alison <apple@apples-MacBook-Pro-2.local>
2022-03-11 14:02:45 -0600

    worked on api abstract + fema api classes

--- api.py
+++ api.py
@@ -8,12 +8,35 @@ March 2022
 
 from abc import ABC, abstractmethod
 
+
 class API(ABC):
 
 	@abstractmethod
-	def get_json_file(self, parameters):
+	def get(self):
 		pass
 
 	@abstractmethod
-	def gen_pandas_dataframe(self):
+	def clean_data(self, dataframes):
 		pass
+
+
+
+"""
+FEMA API
+	Get Method
+		get loops
+		call api
+		return pandas dfs for each dataset
+	Clean Data Method
+		merge data
+		clean anything wrong
+		
+
+Census API
+	Get Method
+		get detail county data
+		get data profile data
+	Clean Data Method
+		merge data
+		cleans data
+"""
--- fema_api.py
+++ fema_api.py
@@ -1,11 +1,23 @@
 """
-API to connect to OpenFEMA.
-
 (la)Monty Python
 Ali Klemencic
 March 2022
 
+API to connect to OpenFEMA.
+
+Downloads data from the Disaster Declaration Summaries, Web
+	Disaster Summaries, and Mission Assignments datasets.
+
 Code adapted from OpenFEMA developer resources: https://www.fema.gov/about/openfema/developer-resources
+
+****************************************************************************************************************
+To Do:
+	Test code to make sure filter paths correct (is it " " or "_"?)
+	Test Pandas dataframe merge code
+	Create virtual environment and run in there
+	Create requirements.txt
+		pip3 freeze > requirements.txt
+*****************************************************************************************************************
 """
 
 import requests
@@ -16,6 +28,9 @@ from api import API
 
 
 class FemaAPI(API):
+	"""
+	Class to create an API connection to OpenFEMA for given states and years.
+	"""
 	base_path = "https://www.fema.gov/api/open"
 	record_count_path = "?$inlinecount=allpages&$select=id&$top=1"
 	top = 1000
@@ -32,21 +47,33 @@ class FemaAPI(API):
 	                        obligationAmount,projectedCompletionDate""")}
 
 	def __init__(self, states, years):
+		"""
+		Constructor.
+
+		:param states: list of states to filter on
+		:param years: list of years to filter on
+		"""
 		self.states = states
 		self.years = years
 		self.disasters = None
-		self.dds_df = None
-		self.wds_df = None
-		self.ms_df = None
+		self.data = pd.DataFrame()
+
 
 	def get_dds_filter_path(self):
+		"""
+		Gets the correct filter path for a DDS dataset API call.
+
+		:return: (str) filter path
+		"""
 		filter_path = "$filter="
+
 		for i, state in enumerate(self.states):
-			filter_path += f'(state_eq_{state}'
+			filter_path += f'(fipsStateCode_eq_{state}'
 			if i == len(states) - 1:
 				filter_path += ")"
 			else:
 				filter_path += "_or_"
+
 		for i, year in enumerate(self.years):
 			if filter_path[-1] == ")":
 				filter_path += "_and_"
@@ -55,41 +82,71 @@ class FemaAPI(API):
 				filter_path += "_or_"
 			else:
 				filter_path += ")"
+
 		return filter_path
 
+
 	def get_wds_ms_filter_path(self):
+		"""
+		Gets the correct filter path for a WDS or MS dataset API call.
+
+		:return: (str) filter path
+		"""
 		filter_path = "$filter="
+
 		for i, num in enumerate(self.disasters):
 			filter_path += f'(disasterNumber_eq_{num}'
 			if i == len(disasters) - 1:
 				filter_path += ")"
 			else:
 				filter_path += "_or_"
+
 		return filter_path
 
+
 	def get_loop_num(cls, dataset, filter_path):
-		# get record count with quick API call
+		"""
+		Class method to get the number of loops required to
+		access every row in the dataset via a quick API call.
+
+		:param dataset: (str) name of the dataset to access
+		:param filter_path: (str) filters for the API call
+
+		:return: (int) number of loops required
+				and (int) total record count
+		"""
 		r = requests.get(
 			cls.base_path
 			+ cls.dataset_dict[dataset][0]
 			+ filter_path
 			+ cls.record_count_path)
+		if r.status_code == 404 or r.status_code == 403:
+			raise ValueError("API call failed")
 		result = r.text.encode("iso-8859-1")
 		json_data = json.loads(result.decode())
+
 		count = json_data['metadata']['count']
 		loop_num = math.ceil(count / TOP)
+
 		return loop_num, count
 
-	def get_json_file(self, parameters):
-		dataset, endpoint, select_path, filter_path, loop_num = parameters
-		# initialize json file
-		filename = dataset + "_output.json"
-		output_file = open(filename, "a")
-		# FEMA website said to pull in as a list under key named that dataset. is this best practice?
-		output_file.write('[')
-		# loop and call API changing record start for each iteration
-		i = 0
+
+	def get_dataframe(self, dataset, filter_path, loop_num):
+		"""
+		Calls the API, looping to get all records, and
+		generates a dataframe from the resulting json data.
+
+		:param dataset: (str) dataset to connect to
+		:param filter_path: (str) filter path
+		:param loop_num: (int) number of iterations required
+
+		:return: Pandas dataframe with resulting API call data
+		"""
+		endpoint, select_path = self.dataset_dict[dataset]
+		dataframe = pd.DataFrame()
+
 		skip = 0
+		i = 0
 		while i < loop_num:
 			r = requests.get(
 				self.base_path
@@ -102,59 +159,58 @@ class FemaAPI(API):
 				+ str(self.top)
 			)
 			result = r.text.encode("iso-8859-1")
+			json_data = json.loads(result.decode())
 
-			# append results to file, trimming off first/last JSONA brackets, adding comma
-			# except for last call, and terminating array bracket and brace on the last call
-			if i == loop_num - 1:
-				output_file.write(str(result[1:-1], 'utf-8') + "]}")
-			else:
-				output_file.write(str(result[1:-1], 'utf-8') + ",")
+			df = pd.DataFrame(json_data)
+			pd.concat([final_dataframe,df])
 
 			i += 1
 			skip = i * self.top
-
-			# announce know what's happening
 			print("Iteration " + str(i) + " done.")
 
-		output_file.close()
-		return filename
+		return dataframe
 
-	def get_json_for_each_dataset(self):
-		filenames = {}
-		for dataset, (endpoint, select_path) in self.dataset_dict.items():
-			# get filters for dataset
+
+	def get(self):
+		"""
+		Gets the data from API calls for each dataset.
+
+		:return: (dict) Pandas dataframes for each dataset
+		"""
+		dataframes = {}
+		keys = ["dds", "wds", "ms"]
+
+		for dataset in keys:
 			if dataset == "dds":
 				filter_path = self.get_dds_filter_path()
 			else:
-				self.disasters = self.dds_df.disasterNumber.unique()
+				self.disasters = dataframes["dds"].disasterNumber.unique()
 				filter_path = self.get_wds_ms_filter_path()
 
 			loop_num, count = self.get_loop_num(dataset, filter_path)
 
-			# announce know what's happening
 			print("Starting " + dataset + " call: " + str(count) + " records, " + str(self.top) +
 			      " returned per call, " + str(loop_num) + " iterations needed.")
 
-			parameters = (dataset, endpoint, select_path, filter_path, loop_num)
-			filename = self.get_json_file(parameters)
-			df = pd.read_json(filename)
-			if dataset == "dds":
-				self.dds_df = df
-			elif datset == "wds":
-				self.wds_df = df
-			elif dataset == "ms":
-				self.ms_df = df
+			df = self.get_dataframe(dataset, filter_path, loop_num)
+			dataframes["dataset"] = df
 
-	def gen_pandas_dataframe(self):
-		self.get_json_for_each_dataset()
+		return dataframes
 
-		interim_df = self.dds_df.join(self.wds_df.set_index('disasterNumber'), on='disasterNumber')
-		final_df = interim_df.join(self.ms_df.set_index('disasterNumber'), on='disasterNumber')
 
-		return final_df
+	def clean_data(self, dataframes):
+		"""
+		Merges data returned by the API calls.
+
+		:param dataframes: (dict) Pandas dataframes
+		                    for each dataset
+		"""
+		for df in dataframes:
+			self.data.join(df.set_index('disasterNumber'), on='disasterNumber')
 
 
 def make_fema_api_call(states, years):
 	api_call = FemaAPI(states, years)
-	dataframe = api_call.gen_pandas_dataframe()
-	return dataframe
+	dataframes = api_call.get()
+	api_call.clean_data(dataframes)
+	return api_call.data
